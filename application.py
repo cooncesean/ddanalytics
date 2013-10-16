e3f0a69036b57dd809e723eba20332f8fb0c2bf3
cooncesean <cooncesean@gmail.com>
2013-10-16 12:49:31 -0700

    Adding raven + sentry to capture errors.

--- application.py
+++ application.py
@@ -5,13 +5,22 @@ use of the `application` var instead of `app`.
 """
 from flask import Flask, render_template
 from flask_login import LoginManager
+from raven.contrib.flask import Sentry
+
 application = Flask(__name__)
+sentry = Sentry(
+    application,
+    dsn='https://cf84591bc153450c9a0ca18e35616d5d:3c1ec9c846da43dbb2981f8ce6a1e7db@app.getsentry.com/4404'
+)
 
 @application.route('/test')
 def test():
     return render_template('home.html')
 
 from ddanalytics.views import *
+
+
+
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
