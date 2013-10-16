88b00569cc687aac34d002446cd8c60da5a5dca7
cooncesean <cooncesean@gmail.com>
2013-10-16 11:09:06 -0700

    Adding `application.py` to test if this is what beanstalk expects.

---
+++ application.py
@@ -0,0 +1,10 @@
+import flask
+
+application = flask.Flask(__name__)
+
+@application.route('/')
+def hello_world():
+    return "Hello world!"
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', debug=True)
