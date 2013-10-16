93345b34525a9408496afebf6194a1b84921a2f5
cooncesean <cooncesean@gmail.com>
2013-10-16 12:11:28 -0700

    Adding python config file to serve static on eb. Modified application.py to see if a test view works.

--- application.py
+++ application.py
@@ -1,5 +1,9 @@
 from ddanalytics import app as application, login_manager
-from ddanalytics.views import *
+# from ddanalytics.views import *
+
+@application.route('/test')
+def test():
+    return 'test works'
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
