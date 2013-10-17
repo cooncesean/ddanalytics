ba3c8d2767edc81d3e034fd87b013b12f542f441
cooncesean <cooncesean@gmail.com>
2013-10-16 23:19:00 -0700

    Raising an exception in application.py to show sys.path on eb.

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+raise Exception(sys.path)
 
 from flask import Flask
 from flask_environments import Environments
